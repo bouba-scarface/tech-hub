e0dcd045166bee4769f67667c9d90b58c6927da4
Guillaume Meyer <gmeyer@salestim.com>
2020-05-26 22:18:44 -0700

    build: v0.0.10

--- src/site/.vuepress/components/SwaggerViewer.vue
+++ src/site/.vuepress/components/SwaggerViewer.vue
@@ -1,48 +1,24 @@
 <template>
   <div>
+    <script
+      type="text/javascript"
+      src="https://unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js"
+    ></script>
     <div id="swagger-ui"></div>
   </div>
 </template>
 
-  <script>
-    export default {
-      mounted() {
+<script>
+export default {
+  mounted () {
     // Retreive prop value
-    var pOpenApiFileUrl = this.openApiFileUrl;
+    var pOpenApiFileUrl = this.openApiFileUrl
 
-    // Inject swagger ui css
-    const bundleCss = document.createElement("link");
-    bundleCss.setAttribute("rel", "stylesheet");
-    bundleCss.setAttribute("type", "text/css");
-    bundleCss.setAttribute(
-      "href",
-      "https://prd.salestim.io/swaggeruiassets/swagger-ui.css"
-    );
-    document.head.appendChild(bundleCss);
-
-    // Inject swagger ui js
-    const pluginBundle = document.createElement("script");
-    pluginBundle.setAttribute(
-      "src",
-      "https://prd.salestim.io/swaggeruiassets/swagger-ui-bundle.js"
-    );
-    pluginBundle.async = true;
-    document.head.appendChild(pluginBundle);
-
-    // Inject swagger ui presets
-    const pluginPreset = document.createElement("script");
-    pluginPreset.setAttribute(
-      "src",
-      "https://prd.salestim.io/swaggeruiassets/swagger-ui-standalone-preset.js"
-    );
-    pluginPreset.async = true;
-    document.head.appendChild(pluginPreset);
-
-    window.onload = function() {
+    window.onload = function () {
       // Begin Swagger UI call region
       const ui = SwaggerUIBundle({
-      url: pOpenApiFileUrl,
-        dom_id: "#swagger-ui",
+        url: pOpenApiFileUrl,
+        dom_id: '#swagger-ui',
         deepLinking: false,
         displayOperationId: false,
         displayRequestDuration: true,
@@ -50,15 +26,13 @@
         plugins: [SwaggerUIBundle.plugins.DownloadUrl]
         // ,
         // layout: 'StandaloneLayout'
-      });
-      // End Swagger UI call region
+      })
 
-      window.ui = ui;
-    };
+      window.ui = ui
+    }
   },
-  props: ["openApiFileUrl"]
-};
+  props: ['openApiFileUrl']
+}
 </script>
 
-  <style scoped>
-  </style>
+<style scoped></style>
